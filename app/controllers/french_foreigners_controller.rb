8a21ebc94ef166269be7cfcafeeaf12ca57af180
TITOTTO <kuaker26@gmail.com>
2023-01-10 14:26:47 +0100

    FTI - French_foreignerValidate + Views + Controller

--- app/controllers/french_foreigners_controller.rb
+++ app/controllers/french_foreigners_controller.rb
@@ -1,4 +1,5 @@
 class FrenchForeignersController < ApplicationController
+  before_action :find_french_foreigner, only: %i[show edit destroy update]
 
   def index
     @french_foreigners = FrenchForeigner.includes(:country).order("countries.country_fr ASC")
@@ -8,24 +9,62 @@ class FrenchForeignersController < ApplicationController
     
   end
 
+  def edit
+    
+  end
+
   def new
     @countries = Country.all
     @french_foreigner = FrenchForeigner.new
   end
 
   def create
-    @french_foreigner = FrenchForeigner.new(french_foreigner_param)
-
-    if @french_foreigner.save
+    @french_foreigner = FrenchForeigner.new(french_foreigner_params)
+    @french_foreigner.save
+    if @french_foreigner.errors.empty?
+      flash[:success] = "Ambassade créé"
       redirect_to french_foreigner_path(@french_foreigner)
     else
-      render :new
+      flash[:notice] = french_foreigner_errors
+      redirect_to new_french_foreigner_path
     end
+  end
+
+
+  def update
+    @french_foreigner.update(french_foreigner_params)
+    if @french_foreigner.errors.empty?
+      flash[:success] = "Ambassade modifié"
+    else
+      flash[:notice] = french_foreigner_errors
+    end
+
+    redirect_to french_foreigner_path(@french_foreigner)
+  end
+
+  def destroy
+    @french_foreigner.destroy
+    if @french_foreigner.errors.empty?
+      flash[:success] = "Ambassade supprimé"
+    else
+      flash[:notice] = french_foreigner_errors
+    end
+    redirect_to french_foreigners_path
   end
 
   private
 
-  def french_foreigner_param
+  def french_foreigner_params
     params.require(:french_foreigner).permit(:year, :nbr_fr, :country_id)
+  end
+
+  def find_french_foreigner
+    @french_foreigner = FrenchForeigner.find(params[:id])
+  end
+
+  def french_foreigner_errors
+    @errors = []
+    @french_foreigner.errors.each {|error| @errors << error.full_message }
+    @errors
   end
 end
--- app/models/french_foreigner.rb
+++ app/models/french_foreigner.rb
@@ -1,3 +1,10 @@
 class FrenchForeigner < ApplicationRecord
+  validates :year,
+              presence: true,
+              numericality: { only_integer: true, message: "Ce champs doit être une année" }
+  validates :nbr_fr,
+              presence: true,
+              numericality: { only_integer: true, message: "Ce champs doit être un nombre" }
+
   belongs_to :country
 end
